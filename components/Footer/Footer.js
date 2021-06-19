32f32aceb2398e52fc4c2d97afd51834e98e90c2
prerana1821 <prerananawar1@gmail.com>
2021-06-19 13:09:39 +0530

    fix: footer icons

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -12,19 +12,12 @@ export const Footer = () => {
       <ul className={footerStyles.footerList}>
         <li>
           <a className='links' href='mailto: prerananw1@gmail.com'>
-            <Image src='/images/instagram.png' width='40px' height='40px' />
+            <Image src='/images/instagram.svg' width='35px' height='35px' />
           </a>
         </li>
-
         <li>
           <a className='links' href='https://github.com/prerana1821'>
-            <Image src='/images/github.png' width='40px' height='40px' />
-          </a>
-        </li>
-
-        <li>
-          <a className='links' href='https://twitter.com/precodes18'>
-            <Image src='/images/twitter.png' width='40px' height='40px' />
+            <Image src='/images/github.svg' width='35px' height='35px' />
           </a>
         </li>
         <li>
@@ -32,7 +25,12 @@ export const Footer = () => {
             className='links'
             href='https://www.linkedin.com/in/prerana-nawar/'
           >
-            <Image src='/images/instagram.png' width='40px' height='40px' />
+            <Image src='/images/discord.svg' width='42px' height='42px' />
+          </a>
+        </li>
+        <li>
+          <a className='links' href='https://twitter.com/precodes18'>
+            <Image src='/images/twitter.svg' width='35px' height='35px' />
           </a>
         </li>
       </ul>

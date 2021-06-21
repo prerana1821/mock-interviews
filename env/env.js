4c8c29c4df3786604a22f70a6f2ccca48f64bea4
prerana1821 <prerananawar1@gmail.com>
2021-06-21 23:41:53 +0530

    refactor: api url env

--- env/env.js
+++ env/env.js
@@ -1,1 +1,6 @@
 export const API_URL = "https://mock-interviews.vercel.app/";
+
+// export const API_URL =
+//   process.env.NODE_ENV !== "production"
+//     ? "http://localhost:3000"
+//     : "https://mock-interviews.vercel.app/";

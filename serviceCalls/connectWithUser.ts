ea77f5cab4ed9a4fc3b93f449afbbf5e4b1da69c
prerana1821 <prerananawar1@gmail.com>
2022-11-05 16:57:23 +0530

    refactor: create Event function

--- serviceCalls/connectWithUser.ts
+++ serviceCalls/connectWithUser.ts
@@ -62,8 +62,6 @@ export const connectWithUser = async ({
   API_KEY,
   SCOPES,
 }: ConnectWithUserParams): Promise<void> => {
-  console.log({ gapi, CLIENT_ID, API_KEY, SCOPES });
-
   if (authState.token) {
     try {
       interviewSlotDispatch({
@@ -74,6 +72,7 @@ export const connectWithUser = async ({
           },
         },
       });
+
       // const response = await fetch(
       //   `${process.env.API_URL}api/interviewSlot/${authState.user._id}/${interviewId}`,
       //   {
@@ -87,16 +86,8 @@ export const connectWithUser = async ({
       // );
       // const data = await response.json();
 
-      // console.log({ data });
-
-      console.log(1, { window });
-
-      // window.triggerGoogleLoaded = function () {
-      console.log("Cool");
-
       window.gapi.load("client:auth2", () => {
         console.log("loaded client");
-
         window.gapi.client
           .init({
             apiKey: API_KEY,
@@ -106,135 +97,27 @@ export const connectWithUser = async ({
               "App Name that you used in google developer console API",
           })
           .then(function () {
-            // window.gapi.client.load("calendar", "v3", () =>
-            //   console.log("bam!")
-            // );
             GoogleAuth = gapi.auth2.getAuthInstance();
-
-            // Listen for sign-in state changes.
             GoogleAuth.isSignedIn.listen(updateSigninStatus);
-
-            // Handle initial sign-in state. (Determine if user is already signed in.)
-            const user = GoogleAuth.currentUser.get();
             setSigninStatus();
 
             window.gapi.client.load("calendar", "v3", () => {
-              console.log("bam!");
+              const user = GoogleAuth.currentUser.get();
               const isAuthorized = user.hasGrantedScopes(SCOPES);
-              console.log({ isAuthorized });
               if (isAuthorized) {
-                const request = window.gapi.client.calendar?.events.insert({
-                  calendarId: "primary",
-                  resource: event,
-                  conferenceDataVersion: "1",
-                });
-
-                console.log({ request });
-
-                request.execute((reqEvent: any) => {
-                  console.log(reqEvent);
-                  console.log("SUCCESSFUL");
-                  // window.open(reqEvent.htmlLink);
-                });
+                createEvent();
               } else {
-                console.log("NEWS");
                 GoogleAuth.signIn().then(() => {
                   const user = GoogleAuth.currentUser.get();
-                  console.log({ user });
-
                   const isAuthorized = user.hasGrantedScopes(SCOPES);
-                  console.log(2, { isAuthorized });
                   if (isAuthorized) {
-                    const request = window.gapi.client.calendar?.events.insert({
-                      calendarId: "primary",
-                      resource: event,
-                      conferenceDataVersion: "1",
-                    });
-
-                    console.log({ request });
-
-                    request.execute((reqEvent: any) => {
-                      console.log(reqEvent);
-                      console.log("SUCCESSFUL");
-                      // window.open(reqEvent.htmlLink);
-                    });
+                    createEvent();
                   }
                 });
               }
             });
-
-            // else {
-            //   GoogleAuth.signIn();
-            // }
-            // Call handleAuthClick function when user clicks on
-            //      "Sign In/Authorize" button.
-            // $("#sign-in-or-out-button").click(function () {
-            //   handleAuthClick();
-            // });
-            // $("#revoke-access-button").click(function () {
-            //   revokeAccess();
-            // });
           });
-
-        // window.gapi.client.load("calendar", "v3", () => console.log("bam!"));
-
-        // window.gapi.auth2
-        //   .getAuthInstance()
-        //   .signIn()
-        //   .then(() => {
-        //     const event = {
-        //       summary: "Awesome Event 2!",
-        //       location: "Mumbai, India",
-        //       description: "Really great refreshments",
-        //       start: {
-        //         dateTime: "2022-11-05T09:00:00-07:00",
-        //         timeZone: "America/Los_Angeles",
-        //       },
-        //       end: {
-        //         dateTime: "2022-11-05T17:00:00-07:00",
-        //         timeZone: "America/Los_Angeles",
-        //       },
-        //       attendees: [
-        //         { email: "snehalnawar8263@gmail.com" },
-        //         { email: "prerananawar1@gmail.com" },
-        //       ],
-        //       conferenceData: {
-        //         createRequest: {
-        //           requestId: "zzz",
-        //           conferenceSolutionKey: {
-        //             type: "hangoutsMeet",
-        //           },
-        //         },
-        //       },
-        //       reminders: {
-        //         useDefault: false,
-        //         overrides: [
-        //           { method: "email", minutes: 24 * 60 },
-        //           { method: "popup", minutes: 10 },
-        //         ],
-        //       },
-        //     };
-
-        //     const request = window.gapi.client.calendar?.events.insert({
-        //       calendarId: "primary",
-        //       resource: event,
-        //       conferenceDataVersion: "1",
-        //     });
-
-        //     request.execute((reqEvent: any) => {
-        //       console.log(reqEvent);
-        //       if (data.success && reqEvent.htmlLink) {
-        //         console.log("Nice");
-        //         interviewSlotDispatch({
-        //           type: "UPDATE_INTERVIEW_SLOTS",
-        //           payload: { interviewSlot: data.data },
-        //         });
-        //       }
-        //       // window.open(reqEvent.htmlLink);
-        //     });
-        //   });
       });
-      // };
     } catch (error) {
       console.log({ error });
       interviewSlotDispatch({
@@ -247,43 +130,28 @@ export const connectWithUser = async ({
   }
 };
 
-function handleAuthClick() {
-  if (GoogleAuth.isSignedIn.get()) {
-    // User is authorized and has clicked "Sign out" button.
-    GoogleAuth.signOut();
-  } else {
-    // User is not signed in. Start Google auth flow.
-    GoogleAuth.signIn();
-  }
-}
-
-function revokeAccess() {
-  GoogleAuth.disconnect();
-}
-
 function setSigninStatus() {
   const user = GoogleAuth.currentUser.get();
   const isAuthorized = user.hasGrantedScopes(SCOPES);
   if (isAuthorized) {
     console.log("Signed In");
-    // window.gapi.client.load("calendar", "v3", () => console.log("bam!"));
-    // $("#sign-in-or-out-button").html("Sign out");
-    // $("#revoke-access-button").css("display", "inline-block");
-    // $("#auth-status").html(
-    //   "You are currently signed in and have granted " + "access to this app."
-    // );
   } else {
-    // GoogleAuth.signIn();
     console.log("Signed Out");
-
-    // $("#sign-in-or-out-button").html("Sign In/Authorize");
-    // $("#revoke-access-button").css("display", "none");
-    // $("#auth-status").html(
-    //   "You have not authorized this app or you are " + "signed out."
-    // );
   }
 }
 
 function updateSigninStatus() {
   setSigninStatus();
+}
+
+function createEvent() {
+  const request = window.gapi.client.calendar?.events.insert({
+    calendarId: "primary",
+    resource: event,
+    conferenceDataVersion: "1",
+  });
+  request.execute((reqEvent: any) => {
+    console.log(reqEvent);
+    console.log("SUCCESSFUL");
+  });
 }
